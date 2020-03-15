870966f2627353d9ae1e96375fc6e4a08229e48b
perjohn <p.john@keplervision.eu>
2020-03-15 10:11:18 +0100

    Previous ball position as input, instead of velocity

--- nn/net.py
+++ nn/net.py
@@ -4,6 +4,7 @@ import torch.nn.functional as F
 
 from environment.pong import SCREEN_WIDTH, SCREEN_HEIGHT
 
+
 class Net1Hidden(nn.Module):
 
     def __init__(self, input_dims: int = 6, fc_dims: int = 128):
@@ -31,9 +32,12 @@ class Net2Hidden(nn.Module):
         actions = self.fc3(x)
         return actions
 
+
 def get_model_input(ball_x, ball_y, ball_x_previous, ball_y_previous, own_player_x, opponent_x) -> torch.Tensor:
-    velocity_ball_x = ball_x - ball_x_previous
-    velocity_ball_y = ball_y - ball_y_previous
-    return torch.Tensor([ball_x / SCREEN_WIDTH, ball_y / SCREEN_HEIGHT, own_player_x / SCREEN_WIDTH,
-                         opponent_x / SCREEN_WIDTH, velocity_ball_x / SCREEN_WIDTH,
-                         velocity_ball_y / SCREEN_HEIGHT])
+    return torch.Tensor(
+        [ball_x / SCREEN_WIDTH,
+         ball_y / SCREEN_HEIGHT,
+         ball_x_previous / SCREEN_WIDTH,
+         ball_y_previous / SCREEN_HEIGHT,
+         own_player_x / SCREEN_WIDTH,
+         opponent_x / SCREEN_WIDTH])
